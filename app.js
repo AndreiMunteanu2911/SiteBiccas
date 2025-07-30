34c8689f94dc3f32c8403eecf92159e6b112e3ee
Andrei Munteanu <andreimunteanu7@yahoo.com>
2025-07-30 14:53:46 +0300

    Functionalitate pentru switch butoane Monthly/Yearly

--- app.js
+++ app.js
@@ -2,28 +2,31 @@ console.log("test");
 
 document.querySelector(".page5-buttonMonthly").addEventListener("click", function()
 {
-    console.log("Clicked Monthly");
     const btnYearly= document.querySelector(".page5-buttonYearly");
     const btnMonthly= document.querySelector(".page5-buttonMonthly");
-    if (btnMonthly.style.background === "#FFFFFF"){
-        console.log("Passed if Monthly");
+    if (window.getComputedStyle(btnMonthly).backgroundColor==="rgb(255, 255, 255)"){
         btnMonthly.style.background="#54BD95";
         btnMonthly.style.color="#FFFFFF";
         btnYearly.style.background="#FFFFFF";
         btnYearly.style.color="#000000";
+        document.querySelector(".page5-proTierPrice").textContent=" 8";
+        document.querySelector(".page5-freeTierPrice").textContent=" 0";
+        document.querySelector(".page5-businessTierPrice").textContent=" 16";
     }
+
 });
 
 document.querySelector(".page5-buttonYearly").addEventListener("click", function()
 {
-    console.log("Clicked Yearly");
     const btnMonthly= document.querySelector(".page5-buttonMonthly");
     const btnYearly= document.querySelector(".page5-buttonYearly");
-    if (btnYearly.style.background === "#FFFFFF"){
-        console.log("Passed if Yearly");
+    if (window.getComputedStyle(btnYearly).backgroundColor==="rgb(255, 255, 255)"){
         btnYearly.style.background="#54BD95";
         btnYearly.style.color="#FFFFFF";
         btnMonthly.style.background="#FFFFFF";
         btnMonthly.style.color="#000000";
+        document.querySelector(".page5-proTierPrice").textContent=" 88";
+        document.querySelector(".page5-freeTierPrice").textContent=" 0";
+        document.querySelector(".page5-businessTierPrice").textContent=" 176";
     }
 });
