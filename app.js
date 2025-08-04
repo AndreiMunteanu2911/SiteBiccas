a8a6dab91744f13c89281375ce77dd56c7385520
Andrei Munteanu <andreimunteanu7@yahoo.com>
2025-08-04 10:42:58 +0300

    Fixed some warnings

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ function setEnlarged(whichCard){
     document.querySelector(`#page5-${whichCard}Tier`).classList.add("enlarged");
     document.querySelector(`#page5-${whichCard}Tier`).classList.remove("enlargeable");
 }
-
 function setEnlargeable(whichCard){
     document.querySelector(`#page5-${whichCard}Tier`).classList.remove("enlarged");
     document.querySelector(`#page5-${whichCard}Tier`).classList.add("enlargeable");
 }
-
 function setActive(whichCard){
     document.querySelector(`#page5-${whichCard}Tier`).classList.remove("page5-cardUnselected");
     document.querySelector(`#page5-${whichCard}Tier`).classList.add("page5-cardSelected");
@@ -24,7 +22,6 @@ function setActive(whichCard){
     document.querySelector(`#page5-${whichCard}TierButton`).classList.remove("page5-unselectedButton");
     document.querySelector(`#page5-${whichCard}TierButton`).classList.add("page5-selectedButton");
 }
-
 function setInactive(whichCard){
     document.querySelector(`#page5-${whichCard}Tier`).classList.add("page5-cardUnselected");
     document.querySelector(`#page5-${whichCard}Tier`).classList.remove("page5-cardSelected");
@@ -41,94 +38,68 @@ function setInactive(whichCard){
     document.querySelector(`#page5-${whichCard}TierButton`).classList.add("page5-unselectedButton");
     document.querySelector(`#page5-${whichCard}TierButton`).classList.remove("page5-selectedButton");
 }
-
 document.querySelector(".page5-buttonMonthly").addEventListener("click", function()
 {
     const btnYearly= document.querySelector(".page5-buttonYearly");
     const btnMonthly= document.querySelector(".page5-buttonMonthly");
     if (window.getComputedStyle(btnMonthly).backgroundColor==="rgb(255, 255, 255)"){
-
         btnMonthly.style.background="#54BD95";
         btnMonthly.style.color="#FFFFFF";
         btnYearly.style.background="#FFFFFF";
         btnYearly.style.color="#000000";
-
         document.querySelector("#page5-proTierPrice").textContent=" 8";
         document.querySelector("#page5-freeTierPrice").textContent=" 0";
         document.querySelector("#page5-businessTierPrice").textContent=" 16";
-
         setEnlargeable("free");
         setEnlargeable("pro");
         setEnlargeable("business");
-
         setInactive("free");
         setInactive("pro");
         setInactive("business");
-
     }
 });
-
 document.querySelector(".page5-buttonYearly").addEventListener("click", function()
 {
     const btnMonthly= document.querySelector(".page5-buttonMonthly");
     const btnYearly= document.querySelector(".page5-buttonYearly");
     if (window.getComputedStyle(btnYearly).backgroundColor==="rgb(255, 255, 255)"){
-
         btnYearly.style.background="#54BD95";
         btnYearly.style.color="#FFFFFF";
         btnMonthly.style.background="#FFFFFF";
         btnMonthly.style.color="#000000";
-
         document.querySelector("#page5-proTierPrice").textContent=" 96";
         document.querySelector("#page5-freeTierPrice").textContent=" 0";
         document.querySelector("#page5-businessTierPrice").textContent=" 192";
-
         setEnlargeable("free");
         setEnlargeable("pro");
         setEnlargeable("business");
-
         setInactive("free");
         setInactive("pro");
         setInactive("business");
-
     }
 });
-
 document.querySelector("#page5-freeTier").addEventListener("click", function(){
-
     setEnlarged("free");
     setEnlargeable("pro");
     setEnlargeable("business");
 
-
     setActive("free");
     setInactive("pro");
     setInactive("business");
-
 });
-
 document.querySelector("#page5-proTier").addEventListener("click", function(){
-
     setEnlarged("pro");
     setEnlargeable("free");
     setEnlargeable("business");
-
     setInactive("free");
     setActive("pro");
     setInactive("business");
-
 });
-
 document.querySelector("#page5-businessTier").addEventListener("click", function(){
-
     setEnlarged("business");
     setEnlargeable("pro");
     setEnlargeable("free");
-
     setInactive("free");
     setInactive("pro");
     setActive("business");
-
 });
-
-/* light green - rgba(212, 255, 239, 1)*/
